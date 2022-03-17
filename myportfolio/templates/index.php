38120dcf954afd58b41f7ce45edcbc9c08ba610a
miko <rosalesmikole@gmail.com>
2022-03-17 12:09:40 +0800

    css

--- myportfolio/templates/index.php
+++ myportfolio/templates/index.php
@@ -4,9 +4,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Deployed Application</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@500&display=swap" rel="stylesheet">
+    <style>
+    body {
+        font-family: 'Sora';
+        margin: 0;
+        padding: 0;
+        background-color: #F7F7F7;
+    }
+    .container {
+        position: absolute;
+        margin: auto;
+        top: 30%;
+        width: 100%;
+    }
+
+    .highlight {
+        color: #FFB72B;
+    }
+
+    .name {
+        text-align: center;
+        text-transform: uppercase;
+        font-size: 4em;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        color: #1B1A17;
+        font-weight: 700;
+    }
+    .program {
+        text-align: center;
+        font-size: 1.3em;
+        margin-top: 2px;
+        color: #1B1A17;
+        font-weight: 500;
+    }
+    </style>
 </head>
+
 <body>
-    <h1>Mikole Ken V. Rosales</h1>
-    <h2>Bachelor of Science in Information Technology - 3</h2>
+    <div class="container">
+        <p class="name"><span class="highlight">MIKOLE</span> ROSALES <span class="highlight">ツ</span></p>
+        <p class="program">Bachelor of Science in Information Technology - 3</p>
+    </div>
 </body>
 </html>
