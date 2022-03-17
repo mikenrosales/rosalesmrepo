0d3b68c45a5383cfb2ce08b9c1ed283e928c8d96
miko <rosalesmikole@gmail.com>
2022-03-17 13:23:41 +0800

    Final!

--- myportfolio/templates/index.php
+++ myportfolio/templates/index.php
@@ -3,43 +3,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Deployed Application</title>
+    <title>About Me</title>
+    <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@500&display=swap" rel="stylesheet">
+    <!-- Internal CSS  -->
     <style>
     body {
-        font-family: 'Sora';
         margin: 0;
         padding: 0;
+        font-family: 'Sora';
         background-color: #F7F7F7;
     }
+
     .container {
+        top: 35%;
+        width: 100%;
         position: absolute;
         margin: auto;
-        top: 30%;
-        width: 100%;
     }
 
     .highlight {
+        font-weight: 500;
         color: #FFB72B;
     }
 
     .name {
         text-align: center;
-        text-transform: uppercase;
         font-size: 4em;
-        margin-top: 5px;
-        margin-bottom: 5px;
-        color: #1B1A17;
         font-weight: 700;
+        color: #1B1A17;
+        margin-top: 2.5px;
+        margin-bottom: 2.5px;
     }
+
     .program {
         text-align: center;
         font-size: 1.3em;
-        margin-top: 2px;
-        color: #1B1A17;
         font-weight: 500;
+        color: #1B1A17;
+        margin-top: 2.5px;
     }
     </style>
 </head>
